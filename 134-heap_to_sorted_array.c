397ff771a9a6d33fa9d9aae6bfb5fc4ca7ec7d2c
ajuwong007 <ajuwong007@gmail.com>
2023-03-31 15:40:57 -0700

    task 40

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,53 +1,50 @@
 #include "binary_trees.h"
 
 /**
- * tree_size - measures the sum of heights of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- *
- * Return: Height or 0 if tree is NULL
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: size of the tree
  */
-size_t tree_size(const binary_tree_t *tree)
-{
-	size_t height_l = 0;
-	size_t height_r = 0;
 
-	if (!tree)
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		height_l = 1 + tree_size(tree->left);
+	size = binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right) + 1;
 
-	if (tree->right)
-		height_r = 1 + tree_size(tree->right);
-
-	return (height_l + height_r);
+	return (size);
 }
 
 /**
- * heap_to_sorted_array - converts a Binary Max Heap
+ * heap_to_sorted_array - Converts a Max Binary Heap
  * to a sorted array of integers
- *
- * @heap: pointer to the root node of the heap to convert
- * @size: address to store the size of the array
- *
- * Return: pointer to array sorted in descending order
- **/
+ * @heap: A pointer to the root node of the heap to convert
+ * @size: A pointer to the size of the array
+ * Return: A pointer to the sorted array, or NULL on failure
+ */
+
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int i, *a = NULL;
+	int *array = NULL;
+	size_t i = 0;
 
-	if (!heap || !size)
+	if (heap == NULL || size == NULL)
 		return (NULL);
 
-	*size = tree_size(heap) + 1;
-
-	a = malloc(sizeof(int) * (*size));
-
-	if (!a)
+	*size = binary_tree_size(heap);
+	array = malloc(sizeof(int) * (*size));
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; heap; i++)
-		a[i] = heap_extract(&heap);
+	while (heap != NULL)
+	{
+		array[i] = heap_extract(&heap);
+		i++;
+	}
 
-	return (a);
+	return (array);
 }
